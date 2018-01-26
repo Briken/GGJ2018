4573bc2e2b42cbc75af4f36b71a39252c8d0bd8d
Oliver Morrison <oliverjaymorrison@gmail.com>
2018-01-26 20:23:27 +0000

    Comming prefabs

---
+++ GameJame2018/Assets/Scripts/NPCScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+
+    public string m_name;
+
+    public bool m_isInfected;
+
+    public bool m_letterDelivered;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
