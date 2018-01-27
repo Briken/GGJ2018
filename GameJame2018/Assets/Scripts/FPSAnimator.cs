cb1f07fafd1fb995e6299bde38cf04633f584f5f
Oliver Morrison <oliverjaymorrison@gmail.com>
2018-01-27 23:28:29 +0000

    mucho grande

---
+++ GameJame2018/Assets/Scripts/FPSAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSAnimator : MonoBehaviour {
+
+    public Animation m_walkingAnimation;
+    public Animation m_idleAnimation;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(GetKey.W))
+        {
+
+        }
+
+
+
+	}
+}
