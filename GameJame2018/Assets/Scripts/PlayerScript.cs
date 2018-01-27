c8e72b85d59b2730f64890f0c9a882e7b2644b30
Oliver Morrison <oliverjaymorrison@gmail.com>
2018-01-27 00:48:23 +0000

    added nipple man

--- GameJame2018/Assets/Scripts/PlayerScript.cs
+++ GameJame2018/Assets/Scripts/PlayerScript.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
 
-    private Rigidbody m_rb;
+    //private Rigidbody m_rb;
+
 
 	// Use this for initialization
 	void Start ()
     {
-        m_rb = GetComponent<Rigidbody>();
+       // m_rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            
-        }
+        //m_rb.AddForce(0, -1, 0);
     }
 }
