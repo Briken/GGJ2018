831b63f0bff48aefafdfc742c741f5d3577b35ec
Briken <brokenbriken@gmail.com>
2018-01-27 22:07:50 +0000

    ambience added

---
+++ GameJame2018/Assets/Scripts/GameSceneScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSceneScript : MonoBehaviour {
+
+    public GameObject uiTimer;
+    Text timerText;
+    public float gameTimer;
+    bool isEnding;
+
+	// Use this for initialization
+	void Start () {
+        timerText = uiTimer.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameTimer -= Time.deltaTime;
+        if (gameTimer <= 0 && isEnding == false)
+        {
+            isEnding = true;
+            EndGame();
+        }
+        timerText.text = gameTimer.ToString();
+	}
+
+    void EndGame()
+    {
+
+    }
+}
