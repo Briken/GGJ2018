5a3afa06e67a3ff691b12376f359c206e4f80ea7
Briken <brokenbriken@gmail.com>
2018-01-27 23:23:53 +0000

    ambient bull

---
+++ GameJame2018/Assets/Scripts/AmbientNoiseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientNoiseScript : MonoBehaviour {
+
+
+    public float maxDistance;
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(this.transform.localPosition, GameObject.FindGameObjectWithTag("Player").transform.localPosition);
+        GetComponent<AudioSource>().volume = (1 / dist) * maxDistance;
+        Debug.Log("Dist: " + dist.ToString() + " Vol: " + GetComponent<AudioSource>().volume.ToString());
+    }
+}
