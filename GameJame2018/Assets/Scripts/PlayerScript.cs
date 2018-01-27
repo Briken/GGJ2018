c80343ed604c657324525b45b9ff954610ebba80
Oliver Morrison <oliverjaymorrison@gmail.com>
2018-01-27 14:10:36 +0000

    added new player character

--- GameJame2018/Assets/Scripts/PlayerScript.cs
+++ GameJame2018/Assets/Scripts/PlayerScript.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
 
-    //private Rigidbody m_rb;
 
 
 	// Use this for initialization
 	void Start ()
     {
-       // m_rb = GetComponent<Rigidbody>();
+       
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //m_rb.AddForce(0, -1, 0);
+        
     }
 }
