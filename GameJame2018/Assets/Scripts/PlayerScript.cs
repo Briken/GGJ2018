8ebb512e13a2c6cc00deb90cd1de343208d938f2
Oliver Morrison <oliverjaymorrison@gmail.com>
2018-01-27 00:10:44 +0000

    Player movement

---
+++ GameJame2018/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private Rigidbody m_rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+        }
+    }
+}
